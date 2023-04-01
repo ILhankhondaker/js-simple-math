b0df88dee49804a3f33bbedabbc05de1fa9617da
ILhankhondaker <ilhankhondaker@gmail.com>
2023-04-01 13:18:37 +0600

    js

---
+++ app.js
@@ -0,0 +1,25 @@
+var myName = "Ilhan Khondaker "
+
+// console.log(myName)
+
+var potato = "25";
+// console.log(potato);
+
+var potatoRealPrice = parseInt(potato)
+// console.log(potatoRealPrice)
+
+var tomato = 40;
+// console.log(tomato)
+var totalPrice = (potatoRealPrice + tomato)
+console.log("Total Bazar :", totalPrice)
+
+
+var potato = 25.88;
+var totmato = 40;
+var totalPrice = (potato + tomato);
+var allNumber = parseFloat(totalPrice);
+console.log(allNumber)
+console.log(parseFloat)
+
+
+
---
+++ math.js
@@ -0,0 +1,7 @@
+let potato = 25;
+potato++
+// potato = potato + 10;
+// potato /= 100
+// potato = potato - 5;
+// potato -= 10;
+console.log(potato)
