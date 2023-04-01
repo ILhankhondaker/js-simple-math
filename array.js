b87b6bd52d7889569859580b7af144ce76640c5e
ILhankhondaker <ilhankhondaker@gmail.com>
2023-04-01 14:26:38 +0600

    p

---
+++ array.js
@@ -0,0 +1,18 @@
+// var , let , const ===========
+
+var date = [1, 4, 2023];
+// console.log("Today date ", date)
+
+let developer = ["Thusher Bro", "Ilhan", "Habib", "Habib vair bouu", "Tusher vai bandobi"]
+console.log(developer)
+console.log("Total developer:", developer.length)
+
+console.log(developer.indexOf("Habib vair bouu"))
+console.log(developer[2])
+
+developer[3] = "Ashraf vair Gf"
+
+developer.push = "Habib vair bandobi 🤤"
+
+developer.unshift("Habib vair Thasha bandobi 🤤🤤🤤🤤")
+console.log(developer)
